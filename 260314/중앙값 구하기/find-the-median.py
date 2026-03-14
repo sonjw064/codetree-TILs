fd4ced0dc011e7f0892bcfc6fb85eb1f9323c50f
sonjw064 <sonjw0641@gmail.com>
2026-03-14 22:38:26 +0900

    Title: 중앙값 구하기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260314/중앙값 구하기/find-the-median.py
+++ 260314/중앙값 구하기/find-the-median.py
@@ -3,7 +3,7 @@ A, B, C = map(int,input().split())
 if A < B and A < C :
     if B < C :
         print(B)
-    else: print(B)
+    else: print(C)
 else :
     if (B < C) and (A < C):
         print(A)
