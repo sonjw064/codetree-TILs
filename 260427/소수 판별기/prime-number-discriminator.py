31d3dfc38a0bffeb92202257efa4398a1dbc71f8
sonjw064 <sonjw0641@gmail.com>
2026-04-27 13:40:30 +0900

    Title: 소수 판별기, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260427/소수 판별기/prime-number-discriminator.py
@@ -0,0 +1,15 @@
+# 변수 선언 및 입력
+n = int(input())
+satisfied = True
+
+for i in range(2, n):
+	# 1과 자기자신을 제외한 약수가 전혀 없다면 그 수는 소수입니다.
+    	if n % i == 0:
+        		satisfied = False
+
+                # 출력
+                if satisfied == True:
+                	print("P")
+                    else:
+                    	print("C")
+                        
