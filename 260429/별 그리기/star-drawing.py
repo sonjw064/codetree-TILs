2a608e5940f661fd68179d50205345d7e380d702
sonjw064 <sonjw0641@gmail.com>
2026-04-29 00:09:17 +0900

    Title: 별 그리기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260429/별 그리기/star-drawing.py
+++ 260429/별 그리기/star-drawing.py
@@ -9,6 +9,6 @@ for i in range(1,n+1):
 for i in range(n-1):
     for j in range(i+1):
         print(" ",end="")
-    for j in range(n-i*2,0,-1):
+    for j in range((2*n)-(2*i)-3):
         print("*",end="")
     print()
