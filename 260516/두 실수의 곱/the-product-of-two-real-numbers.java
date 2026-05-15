123ac969d5efdd7326fc91ef68cbdea2581da514
sonjw064 <sonjw0641@gmail.com>
2026-05-16 02:10:55 +0900

    [Passed] 두 실수의 곱 (97ms, 9MB)

---
+++ 260516/두 실수의 곱/the-product-of-two-real-numbers.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        float a = 5.26f;
+        float b = 8.27f;
+        System.out.printf("%.3f",a*b);
+    }
+}
