ca8fb1f46c4aa0552c87271fcfe48223a30e92dd
sonjw064 <sonjw0641@gmail.com>
2026-05-16 02:07:38 +0900

    [Passed] 길이 단위 변환하기 (98ms, 9MB)

---
+++ 260516/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        float a = 30.48f;
+        float b = 160934f;
+        
+        System.out.printf("9.2ft = %.1fcm\n", a * 9.2);
+        System.out.printf("1.3mi = %.1fcm\n", b * 1.3);
+    }
+}
