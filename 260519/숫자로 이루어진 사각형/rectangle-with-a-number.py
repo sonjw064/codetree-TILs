ca48ccad45714e3e1c7d00c67a62cbba5f79a7a7
sonjw064 <sonjw0641@gmail.com>
2026-05-19 08:15:39 +0900

    [Passed] 숫자로 이루어진 사각형 (71ms, 18MB)

---
+++ 260519/숫자로 이루어진 사각형/rectangle-with-a-number.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+def sqr(n):
+    cnt = 0
+    for i in range(n):
+        for j in range(n):
+            if cnt == 9:
+                cnt = 0
+            cnt += 1
+            print(cnt,end=" ")
+        print()
+sqr(n)
