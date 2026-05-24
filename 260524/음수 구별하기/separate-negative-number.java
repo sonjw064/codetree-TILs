4a6ebfb27ebd20ec690673211efac61d22c2c221
sonjw064 <sonjw0641@gmail.com>
2026-05-24 11:48:22 +0900

    [Passed] 음수 구별하기 (134ms, 10MB)

---
+++ 260524/음수 구별하기/separate-negative-number.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a);
+        if (a < 0) {
+            System.out.print("minus");
+        }
+    }
+}
