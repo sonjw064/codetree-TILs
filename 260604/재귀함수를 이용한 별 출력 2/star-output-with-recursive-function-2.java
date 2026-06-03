41006ec12e20d4f008b0c0f7508a5c2bd8679976
sonjw064 <sonjw0641@gmail.com>
2026-06-04 01:20:01 +0900

    [Passed] 재귀함수를 이용한 별 출력 2 (169ms, 11MB)

---
+++ 260604/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void star(int n){
+        if (n == 0){
+            return;
+        }
+        for (int i = 0; i < n; i++){
+        System.out.print("* ");
+        }
+        System.out.println();
+
+        star(n - 1);
+        
+        for (int i = 0; i < n; i++){
+        System.out.print("* ");
+        }
+        System.out.println();
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        star(n);
+    }
+}
