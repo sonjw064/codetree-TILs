022d6e1ca7608e9b33db6d75d4c5304114e9bd26
sonjw064 <sonjw0641@gmail.com>
2026-08-08 23:53:04 +0900

    [Passed] 길이 단위 변환하기 (2ms, 0MB)

---
+++ 260808/길이 단위 변환하기/change-length-unit.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    double a = 30.48;
+    double b = 160934;
+    printf("9.2ft = %.1lfcm\n",9.2*a);
+    printf("1.3mi = %.1lfcm",1.3*b);
+    // Please write your code here.
+    return 0;
+}
