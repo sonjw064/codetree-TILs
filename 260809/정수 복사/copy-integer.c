4c12349d9d33ab6130457d2464dccc57e8d989aa
sonjw064 <sonjw0641@gmail.com>
2026-08-09 00:21:28 +0900

    [Passed] 정수 복사 (2ms, 0MB)

---
+++ 260809/정수 복사/copy-integer.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a = 3;
+    int b = 4;
+    b = a;
+    printf("%d %d\n",a,b);
+    printf("%d",a*b);
+    
+    // Please write your code here.
+    return 0;
+}
